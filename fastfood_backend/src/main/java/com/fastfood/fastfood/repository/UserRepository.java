0a6a622410d9559afec063d30b7423786841ba70
hermann90 <hermannchefouet@gmail.com>
2022-11-15 19:46:53 +0400

    test

--- fastfood_backend/src/main/java/com/fastfood/fastfood/repository/UserRepository.java
+++ fastfood_backend/src/main/java/com/fastfood/fastfood/repository/UserRepository.java
@@ -16,6 +16,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findByEmailId(String emailId);
 
+    public User findByUserId(String userId);
+
+
 
 
 //    public Employee getEmployeeByEmail(String empEmail) {
